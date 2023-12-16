167acad3913ae574d9728b97f1f944d55d1ebf32
Aniket Pawar <aniket@archstore.in>
2023-12-16 17:47:01 +0530

    initial setup

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const options = {
+  method: 'GET',
+  url: 'https://shazam-api6.p.rapidapi.com/shazam/top_tracks_country',
+  headers: {
+    'X-RapidAPI-Key': '1dbfc08cf3msh0dc5eb7349bdf70p1cb960jsn83d2c093a2d1',
+    'X-RapidAPI-Host': 'shazam-api6.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi'
+})
